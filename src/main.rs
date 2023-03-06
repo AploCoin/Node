e2c3d9db7c6cb9db4f845494e1d9aab9c60c7db7
DoctorEenot <dreenot@gmail.com>
2023-03-06 16:37:27 +0200

    fixed logs

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ async fn main() -> errors::ResultSmall<()> {
             .with_thread_names(true),
     );
     let file_appender = tracing_appender::rolling::daily("logs", "node_log");
-    let (non_blocking, _) = tracing_appender::non_blocking(file_appender);
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
     let collector = collector.with(
         fmt::Layer::new()
             .with_writer(non_blocking)
